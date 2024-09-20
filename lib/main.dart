aaf63e9ebc0e2a6d5cae2b2743bf93fbcc0714dd
Xalovald <guilian.bahno@laposte.net>
2024-09-20 11:25:30 +0200

    stateless widget to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:projectmba2/widget/robot/robot.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,116 +18,123 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Container(
-                    width: 100.0, // Largeur du carré
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.red, // Couleur de fond du carré
-                  ),
-                  Container(
-                    width: 100.0, // Largeur du carré
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.green, // Couleur de fond du carré
-                  ),
-                  Container(
-                    width: 100.0, // Largeur du carré
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.blue, // Couleur de fond du carré
-                  ),
-                ],
-              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //   children: <Widget>[
+              //     Container(
+              //       width: 100.0, // Largeur du carré
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.red, // Couleur de fond du carré
+              //     ),
+              //     Container(
+              //       width: 100.0, // Largeur du carré
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.green, // Couleur de fond du carré
+              //     ),
+              //     Container(
+              //       width: 100.0, // Largeur du carré
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.blue, // Couleur de fond du carré
+              //     ),
+              //   ],
+              // ),
+              // SizedBox(height: 50),
+              // Stack(
+              //   children: <Widget>[
+              //     Container(
+              //       width: 200.0,
+              //       height: 200.0,
+              //       color: Colors.blue,
+              //     ),
+              //     Positioned(
+              //       top: 30,
+              //       left: 30,
+              //       right: 30,
+              //       bottom: 30,
+              //       child: Container(
+              //         width: 150.0,
+              //         height: 150.0,
+              //         color: Colors.green,
+              //       ),
+              //     ),
+              //     Positioned(
+              //       top: 140,
+              //       left: 140,
+              //       child: Container(
+              //         width: 50.0,
+              //         height: 50.0,
+              //         color: Colors.red,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // SizedBox(height: 50),
+              // Stack(
+              //   children: <Widget>[
+              //     Container(
+              //       height: 200.0,
+              //       width: 200.0,
+              //       color: Colors.amber,
+              //       child: Stack(
+              //         children: <Widget>[
+              //           Align(
+              //             alignment: Alignment.bottomRight,
+              //             child: Text('bas droite'),
+              //           ),
+              //           Align(
+              //             alignment: Alignment.topLeft,
+              //             child: Text('Haut gauche'),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // SizedBox(height: 50),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //   children: <Widget>[
+              //     Expanded(
+              //         child: Container(
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.red, // Couleur de fond du carré
+              //     )),
+              //     Expanded(
+              //         child: Container(
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.green, // Couleur de fond du carré
+              //     )),
+              //     Expanded(
+              //         child: Container(
+              //       height: 100.0, // Hauteur du carré (égale à la largeur)
+              //       color: Colors.blue, // Couleur de fond du carré
+              //     )),
+              //   ],
+              // ),
+              // SizedBox(height: 10),
+              // Row(
+              //   children: <Widget>[
+              //     Flexible(
+              //         flex: 2,
+              //         child: Container(
+              //           height: 80.0, // Hauteur du carré (égale à la largeur)
+              //           color: Colors.orange, // Couleur de fond du carré
+              //         )),
+              //     Flexible(
+              //         flex: 1,
+              //         child: Container(
+              //           height: 80.0, // Hauteur du carré (égale à la largeur)
+              //           color: Colors.purple, // Couleur de fond du carré
+              //         ))
+              //   ],
+              // ),
+
               SizedBox(height: 50),
-              Stack(
-                children: <Widget>[
-                  Container(
-                    width: 200.0,
-                    height: 200.0,
-                    color: Colors.blue,
-                  ),
-                  Positioned(
-                    top: 30,
-                    left: 30,
-                    right: 30,
-                    bottom: 30,
-                    child: Container(
-                      width: 150.0,
-                      height: 150.0,
-                      color: Colors.green,
-                    ),
-                  ),
-                  Positioned(
-                    top: 140,
-                    left: 140,
-                    child: Container(
-                      width: 50.0,
-                      height: 50.0,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
+
+              // Ajouter ici le widget RobotList
+              Expanded(
+                child: RobotList(), // Affiche la liste des robots nettoyeurs
               ),
-              SizedBox(height: 50),
-              Stack(
-                children: <Widget>[
-                  Container(
-                    height: 200.0,
-                    width: 200.0,
-                    color: Colors.amber,
-                    child: Stack(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text('bas droite'),
-                        ),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text('Haut gauche'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Expanded(
-                      child: Container(
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.red, // Couleur de fond du carré
-                  )),
-                  Expanded(
-                      child: Container(
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.green, // Couleur de fond du carré
-                  )),
-                  Expanded(
-                      child: Container(
-                    height: 100.0, // Hauteur du carré (égale à la largeur)
-                    color: Colors.blue, // Couleur de fond du carré
-                  )),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: <Widget>[
-                  Flexible(
-                      flex: 2,
-                      child: Container(
-                        height: 80.0, // Hauteur du carré (égale à la largeur)
-                        color: Colors.orange, // Couleur de fond du carré
-                      )),
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        height: 80.0, // Hauteur du carré (égale à la largeur)
-                        color: Colors.purple, // Couleur de fond du carré
-                      ))
-                ],
-              )
             ],
           ),
         ),
--- lib/widget/robot/robot.dart
+++ lib/widget/robot/robot.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:projectmba2/domain.dart';
 
-class RobotList extends StatelessWidget {
+class RobotList extends StatefulWidget {
   const RobotList({super.key});
-  // Liste de robots nettoyeurs
+
+  @override
+  _RobotListState createState() => _RobotListState();
+}
+
+class _RobotListState extends State<RobotList> {
   final List<RobotNettoyeur> robots = [
     RobotNettoyeur('Nettoyeur A', 2020, 'Type A'),
     RobotNettoyeur('Nettoyeur B', 2021, 'Type B'),
     RobotNettoyeur('Nettoyeur C', 2022, 'Type C'),
   ];
+
+  void _addRobot() {
+    setState(() {
+      robots.add(RobotNettoyeur('Nouveau Robot', 2023, 'Type X'));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +35,17 @@ class RobotList extends StatelessWidget {
           final robot = robots[index];
           return ListTile(
             title: Text(robot.name),
-            subtitle: Text('Année: ${robot.year}, Type: ${robot.type}'),
+            subtitle: Text('Année: ${robot.year}, Model: ${robot.type}'),
             onTap: () {
               robot.walk(); // Affiche "Je marche !!" dans la console
             },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addRobot,
+        tooltip: 'Ajouter un robot',
+        child: Icon(Icons.add),
       ),
     );
   }
